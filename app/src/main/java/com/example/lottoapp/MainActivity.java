22eaaa8f82dbb0643681be3d197b00ab530cf45f
habilithm97 <habinkim97@gmail.com>
2023-02-24 23:03:23 +0900

    전체 코드 정리

--- app/src/main/java/com/example/lottoapp/MainActivity.java
+++ app/src/main/java/com/example/lottoapp/MainActivity.java
@@ -3,7 +3,6 @@ package com.example.lottoapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -20,114 +19,85 @@ import com.android.volley.toolbox.Volley;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 
-import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
-import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
-    private static final String TAG = "MainActivity";
 
-    Random random;
-    TextView ball1, ball2, ball3, ball4, ball5, ball6, ballList, ballList2;
+    private TextView resultTv, resultTv1;
 
-    HashSet<String> lottoSet = new HashSet<String>(); // 랜덤 숫자 중복 제거를 위해 HashSet 사용
-    ArrayList<String> arrayList = new ArrayList<String>();
+    private HashSet<String> lottoSet = new HashSet<String>(); // 랜덤 숫자 중복 제거를 위한 HashSet 사용
+    private ArrayList<String> arrayList = new ArrayList<String>();
 
-    RequestQueue requestQueue;
+    private RequestQueue requestQueue;
 
-    String num; // 로또 회차 번호
-    String[] nums = {"drwtNo1", "drwtNo2", "drwtNo3", "drwtNo4", "drwtNo5", "drwtNo6", "bnusNo"}; // 당첨번호 배열
-    EditText edt;
+    private String num; // 로또 회차 번호
+    private EditText edt;
+    private String[] nums = {"drwtNo1", "drwtNo2", "drwtNo3", "drwtNo4", "drwtNo5", "drwtNo6", "bnusNo"}; // 당첨번호 배열
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        random = new Random();
+        initView();
+    }
 
-        ball1 = (TextView) findViewById(R.id.ball1);
-        ball2 = (TextView) findViewById(R.id.ball2);
-        ball3 = (TextView) findViewById(R.id.ball3);
-        ball4 = (TextView) findViewById(R.id.ball4);
-        ball5 = (TextView) findViewById(R.id.ball5);
-        ball6 = (TextView) findViewById(R.id.ball6);
-
+    private void initView() {
+        resultTv = (TextView) findViewById(R.id.resultTv);
         Button btn = (Button) findViewById(R.id.btn);
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                RandomBall();
+                randomBall();
             }
         });
 
-        ballList = (TextView) findViewById(R.id.ballList);
-        Button btn2 = (Button) findViewById(R.id.btn2);
-        btn2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                RandomBall2();
-            }
-        });
-
-        ballList2 = (TextView) findViewById(R.id.ballList2);
+        resultTv1 = (TextView)findViewById(R.id.resultTv1);
         edt = (EditText) findViewById(R.id.edt);
-        Button btn3 = (Button) findViewById(R.id.btn3);
-        btn3.setOnClickListener(new View.OnClickListener() {
+        Button btn1 = (Button) findViewById(R.id.btn1);
+        btn1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 requestLottoNums();
             }
         });
 
-        if (requestQueue == null) { // 요청 큐가 없을 경우에만 생성함 -> 여러번 생성되는 것을 방지함
+        if (requestQueue == null) { // 요청큐가 없을 때만 생성(중복 생성 방지)
             requestQueue = Volley.newRequestQueue(getApplicationContext());
         }
     }
 
-    public void RandomBall() {
-        ball1.setText((String.valueOf(random.nextInt(45) + 1)));
-        ball2.setText((String.valueOf(random.nextInt(45) + 1)));
-        ball3.setText((String.valueOf(random.nextInt(45) + 1)));
-        ball4.setText((String.valueOf(random.nextInt(45) + 1)));
-        ball5.setText((String.valueOf(random.nextInt(45) + 1)));
-        ball6.setText((String.valueOf(random.nextInt(45) + 1)));
-    }
-
-    public void RandomBall2() {
-        ballList.setText(""); // 로또 번호가 표시될 텍스트뷰 초기화
+    private void randomBall() {
+        resultTv.setText("");
 
         while (lottoSet.size() < 7) {
-            int num = (int) (Math.random() * 45); // 45까지 난수 생성
-            Log.d(TAG, String.valueOf(num));
-            // 난수가 0이 아니면 HashSet에 추가
+            int num = (int) (Math.random() * 45);
             if (num != 0) {
                 lottoSet.add(num + "");
             }
         }
-        arrayList.addAll(lottoSet); // HashSet을 ArrayList에 담기
-        Collections.sort(arrayList); // ArrayList를 오름차순 정렬 -> 한자리수만 정렬이 안 되는 문제 발생
+        arrayList.addAll(lottoSet);
+        Collections.sort(arrayList); // arrayList를 오름차순 정렬 -> 한자리수만 정렬이 안 되는 문제 발생
 
         for (int i = 0; i < arrayList.size(); i++) {
             if (i < 6) {
-                ballList.append(arrayList.get(i) + "    ");
+                resultTv.append(arrayList.get(i) + "    ");
             } else {
-                ballList.append(" \n 보너스 번호 : " + arrayList.get(i));
+                resultTv.append(" \n 보너스 번호 : " + arrayList.get(i)); // 보너스 번호는 정렬이 필요없는데...
             }
         }
-        // 초기화
         lottoSet.clear();
         arrayList.clear();
     }
 
-    public void requestLottoNums() {
+    private void requestLottoNums() {
         num = edt.getText().toString();
 
-        if (num.equals("")) { // 회차 번호를 입력하지 않았을 경우
+        if (num.equals("")) {
             Toast.makeText(getApplicationContext(), "회차 번호를 입력하세요. ", Toast.LENGTH_SHORT).show();
             return;
         }
@@ -138,15 +108,15 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onResponse(String response) {
                 // JsonObject는 객체를 Json 객체로 바꿔주거나 Json 객체를 새로 만드는 역할
-                JsonObject jsonObject = (JsonObject)JsonParser.parseString(response);
+                JsonObject jsonObject = (JsonObject) JsonParser.parseString(response);
 
                 String data = num + "회차 당첨 번호 : \n";
 
                 for (int i = 0; i < nums.length - 1; i++) { // 당첨번호 배열 크기만큼 반복
                     data += jsonObject.get(nums[i]) + ", "; // 당첨번호 6개와
                 }
-                data += "\n보너스 : " + jsonObject.get(nums[nums.length - 1]); // 보너스 번호를
-                ballList2.setText(data); // 텍스트뷰에 출력
+                data += "\n보너스 번호 : " + jsonObject.get(nums[nums.length - 1]); // 보너스 번호를
+                resultTv1.setText(data); // TextView에 출력
             }
         }, new Response.ErrorListener() {
             @Override
@@ -159,8 +129,6 @@ public class MainActivity extends AppCompatActivity {
             }
         };
         request.setShouldCache(false); // 이전 결과가 있어도 새로 요청하여 응답을 보여줌
-        requestQueue.add(request); // 요청 큐에 요청 객체 넣기 -> 요청 큐가 알아서 요청과 응답 과정 진행
+        requestQueue.add(request); // 요청큐에 요청 객체 넣기 -> 요청 큐가 알아서 요청과 응답 과정 진행
     }
 }
-
-//
